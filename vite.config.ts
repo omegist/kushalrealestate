a2b816ea8065f9a97c43dc3979b6619eb5d98fed
omegist <chavansahil899@gmail.com>
2026-06-27 20:29:10 +0530

    add recent changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterPlugin } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterPlugin(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
